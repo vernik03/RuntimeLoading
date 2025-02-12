8cb43b6994071118d2ac6fda2d9b83c0c603ce51
Nikita Vertelko <vernik03@gmail.com>
2025-02-12 18:16:50 +0100

    initial commit

---
+++ Source/RuntimeLoadingDemo/RuntimeLoadingDemo.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RuntimeLoadingDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RuntimeLoadingDemo, "RuntimeLoadingDemo" );
+ 
